ca3133a890166063ccc40c4366ef04a45b36d95c
XinChan <XinChan365@outlook.com>
2019-04-17 21:13:59 +0800

    dropout

--- learn_dropout.py
+++ learn_dropout.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from sklearn.datasets import load_digits
-from sklearn.cross_vilidation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
 
 # load data
@@ -21,6 +21,7 @@ def add_layer(inputs, in_size,out_size,n_layer,activation_function=None):
 			tf.summary.histogram(layer_name+'biases',biases)
 		with tf.name_scope('Wx_plus_b'):
 			Wx_plus_b = tf.add(tf.matmul(inputs, weights),biases)
+			Wx_plus_b = tf.nn.dropout(Wx_plus_b, keep_prob)
 
 		if activation_function is None:
 			outputs = Wx_plus_b
@@ -30,14 +31,33 @@ def add_layer(inputs, in_size,out_size,n_layer,activation_function=None):
 		return outputs
 
 # define placeholder
+keep_prob = tf.placeholder(tf.float32)
 xs = tf.placeholder(tf.float32,[None, 64])
 ys = tf.placeholder(tf.float32,[None, 10])
 
 # add layer
-l1 = add_layer(xs, 64,100, 'l1', activation_function = tf.nn.tanh)
-prediction = add_layer(xs, 100,10,'l2', activation_function = tf.nn.softmax)
+l1 = add_layer(xs, 64,50, 'l1', activation_function = tf.nn.tanh)
+prediction = add_layer(l1, 50,10,'l2', activation_function = tf.nn.softmax)
 
 # the loss 
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),reduction_indices=[1]))
 tf.summary.scalar('loss', cross_entropy)
-train_step = tf.train.GradientDescentOptimizer(0.6)
+train_step = tf.train.GradientDescentOptimizer(0.6).minimize(cross_entropy)
+
+sess = tf.Session()
+merged = tf.summary.merge_all()
+
+# summary
+train_writer = tf.summary.FileWriter("logs/train", sess.graph)
+test_writer = tf.summary.FileWriter("logs/test", sess.graph)
+
+sess.run(tf.global_variables_initializer())
+
+for i in range(500):
+	sess.run(train_step, feed_dict = {xs:X_train, ys:y_train, keep_prob:0.5})
+	if i % 50 == 0:
+		# record loss
+		train_result = sess.run(merged, feed_dict = {xs:X_train, ys:y_train, keep_prob:1})
+		test_result = sess.run(merged, feed_dict= {xs:X_test, ys:y_test, keep_prob:1})
+		train_writer.add_summary(train_result, i)
+		test_writer.add_summary(test_result, i)
